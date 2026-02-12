35331753b9bfda5290314e9c2e13876f827bbbe5
vxnyaand <oneivan159@gmail.com>
2026-02-13 01:24:06 +0200

    Update ukraine_history_bot.py

--- csharp-training/Lesson01/ukraine_history_bot.py
+++ csharp-training/Lesson01/ukraine_history_bot.py
@@ -3,7 +3,7 @@ from telegram import Poll
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-TOKEN = '8309839168:AAGBeHH5N_UxbBGyOvgPReQHX433uVk52uA'
+TOKEN = '"your_token_here"'
 GROUP_ID = -5148918793
 
 questions = [
